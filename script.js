9ded4bef64b77df83d2f031d25f198b1cafc8ba2
soumya talwar <soumya.talwar97@gmail.com>
2021-08-25 03:12:49 +0530

    finished conclusion

--- script.js
+++ script.js
@@ -1,3 +1,47 @@
+const frames = {
+  1: "images/frames/frame 1.jpg",
+  2: "images/frames/frame 2.jpg",
+  3: "images/frames/frame 3.jpg",
+  4: "images/frames/frame 4.jpg",
+  5: "images/frames/frame 5.jpg",
+  6: "images/frames/frame 6.jpg",
+  7: "images/frames/frame 7.jpg",
+  8: "images/frames/frame 8.jpg",
+  9: "images/frames/frame 9.jpg",
+  10: "images/frames/frame 10.jpg",
+  11: "images/frames/frame 11.jpg",
+  12: "images/frames/frame 12.jpg",
+  13: "images/frames/frame 13.jpg",
+  14: "images/frames/frame 14.jpg",
+  15: "images/frames/frame 15.jpg",
+  16: "images/frames/frame 16.jpg",
+  17: "images/frames/frame 17.jpg",
+  18: "images/frames/frame 18.jpg",
+  19: "images/frames/frame 19.jpg",
+  20: "images/frames/frame 20.jpg",
+  21: "images/frames/frame 21.jpg",
+  22: "images/frames/frame 22.jpg",
+  23: "images/frames/frame 23.jpg",
+  24: "images/frames/frame 24.jpg",
+  25: "images/frames/frame 25.jpg",
+  26: "images/frames/frame 26.jpg",
+  27: "images/frames/frame 27.jpg",
+  28: "images/frames/frame 28.jpg",
+  29: "images/frames/frame 29.jpg",
+  30: "images/frames/frame 30.jpg",
+  31: "images/frames/frame 31.jpg",
+  32: "images/frames/frame 32.jpg",
+  33: "images/frames/frame 33.jpg",
+  34: "images/frames/frame 34.jpg",
+  35: "images/frames/frame 35.jpg",
+  36: "images/frames/frame 36.jpg",
+  37: "images/frames/frame 37.jpg",
+  38: "images/frames/frame 38.jpg",
+  39: "images/frames/frame 39.jpg",
+  40: "images/frames/frame 40.jpg"
+};
+let play = false;
+
 var sex, id, exp;
 var code = {};
 var sex1, sex2, choice;
@@ -67,6 +111,23 @@ var content = {
 }
 
 $(document).ready(() => {
+  $(window).scroll(() => {
+    let index = Math.min(Math.floor(window.scrollY / 50) + 1, 40);
+    let image = frames[index];
+    $("#image-container").css("background-image", `url("${image}")`);
+  });
+
+  let audio = document.getElementById("music");
+  let button = $("#play-button");
+  button.click(() => {
+    if (!play)
+      audio.play();
+    else
+      audio.pause();
+    play = !play;
+    button.toggleClass(["fa-play", "fa-pause"]);
+  });
+
   $("a").each((index, link) => {
     link.addEventListener("click", event => {
       if (link.hash !== "") {
@@ -167,8 +228,21 @@ $(document).ready(() => {
       let text3 = "<u>gender expression : " + code.exp + " (" + exp + ")</u><br><br>" + content.exp[exp];
       $("#result li").eq(2).html(text3);
       $("#result").removeClass("d-none");
+      $("#conclusion").removeClass("d-none");
     }, true);
   });
+
+  let matrix = $("#matrix");
+  let button2 = $("#toggle-matrix");
+  button2.click(() => {
+    if (matrix.hasClass("d-none"))
+      button2.text("hide matrix");
+    else
+      button2.text("show matrix");
+    matrix.toggleClass(["d-none", "col-12", "col-lg-10"]);
+  });
+
+  $("#show-matrix").click(() => $("#glossary").removeClass("d-none"));
 
 });
 
